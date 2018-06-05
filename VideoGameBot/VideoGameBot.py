26f624858f7e3e8484b96fefa5ee62cdae0a0dc9
sannisthsoni <soni.sannisth@gmail.com>
2018-06-04 17:06:24 -0700

    Universe needs working. Doesn't seem to import. Docker setup needed.

--- VideoGameBot/VideoGameBot.py
+++ VideoGameBot/VideoGameBot.py
@@ -1,1 +1,14 @@
+# !/usr/bin/python
 #From https://www.youtube.com/watch?v=mGYU5t8MO7s
+
+import gym
+import universe
+
+env = gym.make('flashgames.CoasterRacer-v0')
+observation_n = env.reset()
+
+while True:
+    action_n = [[('KeyEvent','ArrowUp',True)] for ob in observation_n]
+    obervation_n, reward_n, done_n, info = env.step(action_n)
+    env.render()
+
